2c615acd3b405a8dfa4ea5dfa1ed461352bdbcd2
tim <gukasovtimofej@gmail.com>
2024-07-25 15:43:38 +0300

    Added production and development webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,58 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const {readFileSync} = require("fs");
+const Dotenv = require('dotenv-webpack');
+const fs = require("node:fs");
+
+module.exports = {
+    entry: [
+        './src/index.tsx',
+    ],
+    output: {
+
+        filename: 'bundle.js', // Имя выходного файла сборки
+        path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+        publicPath: '',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+                use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+                type: 'asset',
+
+            },
+
+        ],
+    },
+
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: "static", to: 'static'},
+            ],
+        }),
+        new Dotenv()
+    ],
+
+    mode: 'production', // Режим сборки
+};
